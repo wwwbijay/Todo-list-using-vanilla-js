5656c7646e135072d187a94b45a7ddbfc54fa772
wwwbijay <link2bj@gmail.com>
2023-01-31 22:05:10 +0545

    todo checked feature added

--- main.js
+++ main.js
@@ -50,4 +50,9 @@ function deleteCheck(e){
     const todo = item.parentElement;
     todo.remove();
   }
+
+  if(item.classList[0]==='complete-btn'){
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
 }
